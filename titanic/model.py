5d61c200ebe5ce6e640d2cfd74cdadc5b11784e8
hahnicity <grehm87@gmail.com>
2015-06-03 00:53:36 -0700

    add a cv set. A step forward. Play with the parameters a bit

--- titanic/model.py
+++ titanic/model.py
@@ -15,8 +15,11 @@ def get_training_data(data_set):
 
 def get_data(data_set):
     data_set["Sex"] = data_set["Sex"] == "male"
-    data_set.Age = data_set.Age.map(lambda x: -1 if numpy.isnan(x) else x)
-    data_set.Fare = data_set.Fare.map(lambda x: -1 if numpy.isnan(x) else x)
+    #data_set.Age = data_set.Age.map(lambda x: -1 if numpy.isnan(x) else x)
+    #data_set.Fare = data_set.Fare.map(lambda x: -1 if numpy.isnan(x) else x)
+    del data_set["Parch"]
+    del data_set["Age"]
+    del data_set["Fare"]
     del data_set["Name"]
     del data_set["PassengerId"]
     del data_set["Ticket"]
@@ -37,10 +40,17 @@ def write_output(predictions):
 def main():
     working_dir = dirname(abspath(__file__))
     training_path = join(working_dir, "train.csv")
-    training_set, y = get_training_data(read_csv(training_path))
+    x, y = get_training_data(read_csv(training_path))
+    two_thirds = int(len(x) * (2.0 / 3))
+    cv_set = x[two_thirds:]
+    y_cv = y[two_thirds:]
+    training_set = x[:two_thirds]
+    y_train = y[:two_thirds]
     classifier = RandomForestClassifier()
-    classifier.fit(training_set, y)
-    testing_path = join(working_dir, "test.csv")
-    testing_set = get_data(read_csv(testing_path))
-    predictions = classifier.predict(testing_set)
-    write_output(predictions)
+    classifier.fit(training_set, y_train)
+    res = classifier.score(cv_set, y_cv)
+    import pdb; pdb.set_trace()
+#    testing_path = join(working_dir, "test.csv")
+#    testing_set = get_data(read_csv(testing_path))
+#    predictions = classifier.predict(testing_set)
+#    write_output(predictions)
